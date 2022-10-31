1e21cc1bd68d59d32f0597a0c33b718af0250515
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-31 22:48:59 +0530

    outputting lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
+import { useState } from "react"
 
-    const handleClick = ()=> {
-        console.log('hi bro')
-    }
-    const handleClickAgain = (name)=> {
-        console.log('hello there')
-    }
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author:'vinay', id: 1},
+        { title: 'Welcome', body: 'lorem ipsum...', author:'adarsh', id: 2},
+        { title: 'web dev tips', body: 'lorem ipsum...', author:'subu', id: 3},
+    ]);
+
+
     return(
         <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={handleClickAgain}>Click Me Again</button>
-
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     )
 }
