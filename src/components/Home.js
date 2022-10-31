ea90dedb2afac5623bcdfcf712939344fd7b3bb8
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-31 21:56:27 +0530

    click events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+
+    const handleClick = ()=> {
+        console.log('hi bro')
+    }
+    const handleClickAgain = (name)=> {
+        console.log('hello there')
+    }
     return(
         <div className="Home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={handleClickAgain}>Click Me Again</button>
 
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,11 @@ const Navbar = () => {
             <h1>Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: '#f1356d',
+                    borderRadius:'8px'
+                }}>New Blog</a>
             </div>
         </nav>
     )
