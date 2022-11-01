2905e276942b66cfc92f59004db604185e55dcc2
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-01 12:29:15 +0530

    BLOG APP USING REACT

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius:'8px'
-                }}>New Blog</a>
-            </div>
-        </nav>
+      <h1>Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
     )
 }
 
