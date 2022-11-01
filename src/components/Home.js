c7e2f398b68048f55f8002aac746365aaac054b3
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-01 09:23:45 +0530

    conditional loading message

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,34 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const[isPending, setIsPending] = useState(true);
 
+    // useEffect(() => {
+    //   fetch('http://localhost:3000/blogs')
+    //     .then(res => {
+    //       return res.json();
+    //     })
+    //     .then(data => {
+    //       setBlogs(data);
+    //       setIsPending(false);
+    //     })
+    // }, [])
     useEffect(() => {
-      fetch('http://localhost:3000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data);
-        })
-    }, [])
-  
+        setTimeout(()=>{
+            fetch('http://localhost:3000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            })
+        },1000)
+      }, [])
     return (
       <div className="home">
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} />}
       </div>
     );
