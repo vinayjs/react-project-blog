88ae07531c4221b8b7aff187041bd684e783be28
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-01 10:33:44 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
      <div className='content'>
-      <Home />
+      <Switch> 
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
      </div>
     </div>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
+
 const Home = () => {
     const { data :blogs, isPending, error} = useFetch('http://localhost:3000/blogs')
  
