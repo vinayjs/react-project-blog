6dbccf1ba7206f7f1860ce865aa924983a89df71
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-01 09:37:00 +0530

    catching errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,34 +3,36 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    const[isPending, setIsPending] = useState(true);
-
-    // useEffect(() => {
-    //   fetch('http://localhost:3000/blogs')
-    //     .then(res => {
-    //       return res.json();
-    //     })
-    //     .then(data => {
-    //       setBlogs(data);
-    //       setIsPending(false);
-    //     })
-    // }, [])
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+  
     useEffect(() => {
-        setTimeout(()=>{
-            fetch('http://localhost:3000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-        },1000)
-      }, [])
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if (!res.ok) { // error coming back from server
+            throw Error('could not fetch the data for that resource');
+          } 
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setBlogs(data);
+          setError(null);
+        })
+        .catch(err => {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        })
+      }, 1000);
+    }, [])
+  
     return (
       <div className="home">
-        {isPending && <div>Loading...</div>}
-        {blogs && <BlogList blogs={blogs} />}
+        { error && <div>{ error }</div> }
+        { isPending && <div>Loading...</div> }
+        { blogs && <BlogList blogs={blogs} /> }
       </div>
     );
   }
